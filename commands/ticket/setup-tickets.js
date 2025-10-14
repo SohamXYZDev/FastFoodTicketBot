0a2289d9c6c0d28e55a04e7a06260be5405cb844
FallLikeAThunderbolt <extremelycoolwindow101@gmail.com>
2025-10-14 20:14:51 +0530

    Fix thumbnail URL to use correct image from JSON

--- commands/ticket/setup-tickets.js
+++ commands/ticket/setup-tickets.js
@@ -23,7 +23,7 @@ module.exports = {
             .setTitle('Start Order')
             .setDescription('**How to Order**\nClick the button below and complete the form to place your order!\n\nPayment Methods Accepted:\n-# PayPal · Venmo · Apple Pay · Zelle · Cash App · Crypto')
             .setColor('#005EFF')
-            .setThumbnail('https://media.discordapp.net/attachments/1424068610355363963/1427324660810256474/ChatGPT_Image_Oct_13_2025_03_11_27_AM.png');
+            .setThumbnail('https://i.ibb.co/fYkrgwKy/Chat-GPT-Image-Oct-13-2025-12-09-59-PM.png');
 
         // Create buttons
         const row = new ActionRowBuilder()
